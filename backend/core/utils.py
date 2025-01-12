ea5ac96eba2cbbdfdd6b72259805b5e6052901a2
Vladislav Klimenko <vlad991199@yandex.ru>
2025-01-12 18:49:01 +0300

    Crud teset for items

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,6 +1,10 @@
 import random
 import string
+import uuid
 
 
-def random_str(length: int = 8):
+def random_str(length: int = 8) -> str:
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+
+def path_with_id(router_path: str, id: int | uuid.UUID) -> str:
+    return router_path + '/' + str(id)
--- backend/features/items/test_items.py
+++ backend/features/items/test_items.py
@@ -2,8 +2,7 @@ from httpx import AsyncClient
 import pytest
 from . import schemas, router
 from core.fixtures import *
-from core.utils import random_str
-
+from core.utils import random_str, path_with_id
 
 async def test_read_items(client: AsyncClient):
     response = await client.get(router.ROUTER_PATH)
@@ -14,8 +13,10 @@ async def test_create_item(client: AsyncClient):
     read_response = await client.get(router.ROUTER_PATH)
     old_quantity = int(read_response.json()['total_count'])
     assert old_quantity >= 0
-    new_item = schemas.ItemCreateSchema(name=random_str(), description=random_str())
-    create_response = await client.post(router.ROUTER_PATH, json=new_item.model_dump())
+    new_item = schemas.ItemCreateSchema(name=random_str(), 
+                                        description=random_str())
+    create_response = await client.post(router.ROUTER_PATH, 
+                                        json=new_item.model_dump())
     assert create_response.status_code == 200
 
     read_response = await client.get(router.ROUTER_PATH)
@@ -26,9 +27,25 @@ async def test_create_item(client: AsyncClient):
 async def test_crud_item(client: AsyncClient):
     new_item = schemas.ItemCreateSchema(name=random_str(), description=random_str())
     create_response = await client.post(router.ROUTER_PATH, json=new_item.model_dump())
-    assert create_response.status_code == 200
     created_item_dict = create_response.json()
     created_item = schemas.ItemSchema(**created_item_dict)
     assert new_item.name == created_item.name
+
+    updating_item = schemas.ItemUpdateSchema(**new_item.model_dump())
+    updating_item.name = random_str()
+    id_path = path_with_id(router.ROUTER_PATH, created_item.id)
+    update_response = await client.patch(id_path, 
+                                         json=updating_item.model_dump())
+    assert update_response.status_code == 200
+
+    read_response = await client.get(id_path)
+    updated_item = schemas.ItemSchema(**read_response.json())
+    assert updated_item.name == updating_item.name
+
+    remove_response = await client.delete(id_path)
+    assert remove_response.status_code == 200
+
+    read_response = await client.get(id_path)
+    assert read_response != 200
 
     
